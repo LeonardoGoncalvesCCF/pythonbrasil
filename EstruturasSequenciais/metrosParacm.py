9968dcf7ec26144ed025221dbaf72daf4e6dfee5
Leonardo <leonardogccf@gmail.com>
2023-10-17 18:40:00 -0300

    metros para cm

---
+++ EstruturasSequenciais/metrosParacm.py
@@ -0,0 +1,7 @@
+#Faça um Programa que converta metros para centímetros.
+print("Tranformação metros para centimetros")
+metros = float(input("Digite o valor que vc quer converter de meyros para cm: "))
+
+cm = metros*100
+
+print("O valor em cm é de ", cm)
