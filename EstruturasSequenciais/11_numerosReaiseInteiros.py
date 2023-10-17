7d39222bae51c707e44f3e99d7e1a4b2a1ef2f0b
Leonardo <leonardogccf@gmail.com>
2023-10-17 20:38:21 -0300

    numeros reais e inteiros

---
+++ EstruturasSequenciais/11_numerosReaiseInteiros.py
@@ -0,0 +1,60 @@
+#Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+    # (A) o produto do dobro do primeiro com metade do segundo.
+    # (B) a soma do triplo do primeiro com o terceiro.
+    # (C) o terceiro elevado ao cubo.
+    
+
+print("------------------------------------------------------------")
+print("Digite dois numeros inteiros e um numero real")
+print("------------------------------------------------------------")
+num1 = int(input(" Primeiro numero inteiro: "))
+num2 = int(input(" Segundo numeor inteiro: "))
+print("------------------------------------------------------------")
+num3 = float(input(" Numeor real: "))
+print("------------------------------------------------------------")
+
+#A) o produto do dobro do primeiro com metade do segundo.
+a = num1 * 2 * (num2/2)
+print("O produto do dobro do primeiro com metade do segundo é igual a: ",a)
+print("------------------------------------------------------------")
+
+
+#B) a soma do triplo do primeiro com o terceiro.
+b = num1 * 3 + num3
+print("A soma do triplo do primeiro com o terceiro é igual a: ",b)
+print("------------------------------------------------------------")
+
+#C) o terceiro elevado ao cubo
+c = num3 ** 3 #** significa elevado
+print("O terceiro elevado ao cubo é igual a: ", c)
+print("------------------------------------------------------------")
+#Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+    # (A) o produto do dobro do primeiro com metade do segundo.
+    # (B) a soma do triplo do primeiro com o terceiro.
+    # (C) o terceiro elevado ao cubo.
+    
+
+print("------------------------------------------------------------")
+print("Digite dois numeros inteiros e um numero real")
+print("------------------------------------------------------------")
+num1 = int(input(" Primeiro numero inteiro: "))
+num2 = int(input(" Segundo numeor inteiro: "))
+print("------------------------------------------------------------")
+num3 = float(input(" Numeor real: "))
+print("------------------------------------------------------------")
+
+#A) o produto do dobro do primeiro com metade do segundo.
+a = num1 * 2 * (num2/2)
+print("O produto do dobro do primeiro com metade do segundo é igual a: ",a)
+print("------------------------------------------------------------")
+
+
+#B) a soma do triplo do primeiro com o terceiro.
+b = num1 * 3 + num3
+print("A soma do triplo do primeiro com o terceiro é igual a: ",b)
+print("------------------------------------------------------------")
+
+#C) o terceiro elevado ao cubo
+c = num3 ** 3 #** significa elevado
+print("O terceiro elevado ao cubo é igual a: ", c)
+print("------------------------------------------------------------")
