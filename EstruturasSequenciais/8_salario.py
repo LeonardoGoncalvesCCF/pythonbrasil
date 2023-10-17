6b3eb57120a05ac63d63db5336ed2d300eca9bd5
Leonardo <leonardogccf@gmail.com>
2023-10-17 19:20:15 -0300

    salario calculado

---
+++ EstruturasSequenciais/8_salario.py
@@ -0,0 +1,10 @@
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+print("Para calcular o salario é preciso saber quantas horas voçê trabalhou e qual o valor da hora")
+print("------------------------------------------------------------")
+horas = float(input("Quantas horas você trabalhar por mes? "))
+print("------------------------------------------------------------")
+reais = float(input("Qauntos reais voçê ganha por hora? "))
+print("------------------------------------------------------------")
+salario = horas * reais
+print("Seu salrio é de: ", salario)
+print("------------------------------------------------------------")
