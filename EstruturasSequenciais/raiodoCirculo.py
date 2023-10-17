0646c38b3249243c20f0b7545ba5f4224f1b6174
Leonardo <leonardogccf@gmail.com>
2023-10-17 18:56:48 -0300

    area, diamentro e raio de um circulo

---
+++ EstruturasSequenciais/raiodoCirculo.py
@@ -0,0 +1,15 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+print("------------------------------------------------------------")
+print ("Calculando o Raio de um Circulo para mostrar a Area total")
+print("------------------------------------------------------------")
+raio = float(input("informe o raio do Circulo: "))
+print("------------------------------------------------------------")
+diametro = raio * 2
+area = 3.14*(raio**2)
+print(" o Diametro é igual a:", diametro )
+print("------------------------------------------------------------")
+print("A area do Circulo é de", area)
+print("------------------------------------------------------------")
+
+
