2f3da875de7f517d6456aececffaaae854806cdb
Leonardo <leonardogccf@gmail.com>
2023-10-17 19:11:56 -0300

    area quadrado

---
+++ EstruturasSequenciais/7_areadoQuadrado.py
@@ -0,0 +1,13 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+print("------------------------------------------------------------")
+print("Informe a base e a altura do quadrados para calcular sua area")
+print("------------------------------------------------------------")
+altura = float(input("Informe a altura do Quadrado: "))
+print("------------------------------------------------------------")
+base = float(input("Informe a base do Quadrado: "))
+print("------------------------------------------------------------")
+
+area = base * altura
+
+print("A area do Quadrado é: ", area )
