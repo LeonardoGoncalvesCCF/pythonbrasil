53a88e800fb0d34bb21d95f170f9d28a0b515d43
Leonardo <leonardogccf@gmail.com>
2023-10-17 20:37:12 -0300

    temperatura de C para F

---
+++ EstruturasSequenciais/10_temperatura2.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+print("------------------------------------------------------------")
+tempc = float(input("Qual a temperatura em Celsius? "))
+print("------------------------------------------------------------")
+
+tempf = 1.8*tempc + 32
+
+print("A temperatura em Fahrenheit é de: ", tempf)
